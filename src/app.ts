a98fe67e95e9f5ab450ea240620d53bed92b4787
Carolyn Mei <mcarolyn@google.com>
2019-08-14 16:45:42 -0700

    Delete unused public folder

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 const app: express.Application = express();
 import { index } from './routes/index';
 
-app.use(express.static('public'));
 app.set('view engine', 'pug')
 
 app.use('/', index);
