b6f77787c24e938f8dc108e9b2a715801df35037
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-13 18:33:06 -0700

    Set up gts for google typscript stuff

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
-import express from "express";
+import express from 'express';
 const app: express.Application = express();
 
 function typeAnnotationTest(name: string) {
   return `Hello ${name}!`;
 }
 
-const list = ["a", "b", "c"];
+app.get('/', (req, res) => {
+  console.log('Hello world received a request.');
 
-app.get("/", (req, res) => {
-  console.log("Hello world received a request.");
-
-  const name: string = req.ip || "World";
+  const name: string = req.ip || 'World';
   res.send(typeAnnotationTest(name));
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log("Server listening on port", port);
+  console.log('Server listening on port', port);
 });
