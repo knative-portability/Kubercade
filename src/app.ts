a978d94f6901cc0f55e7b8f6639556b56d06c67e
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-15 15:02:51 -0700

    Add and serve basic nexus

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 const app: express.Application = express();
 import { index } from './routes/index';
+import { nexus } from './routes/nexus';
 import { scores } from './routes/scores';
 
 app.set('view engine', 'pug');
 
-app.set('view engine', 'pug');
-
 app.use('/', index);
+app.use('/nexus', nexus);
 app.use('/scores', scores);
 
 const port: number = Number(process.env.PORT) || 8080;
