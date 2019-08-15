c488564428b9ed4a194f84122e61c0398f34118e
Carolyn Mei <mcarolyn@google.com>
2019-08-15 15:38:24 -0700

    Restructure game serving for easier adding of new games

--- src/controllers/gameController.ts
+++ src/controllers/gameController.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 
-function pacman(req: express.Request, res: express.Response) {
-  res.redirect('/static/pacman/index.html');
-}
+const gameServes = {
+  pacman(req: express.Request, res: express.Response) {
+    res.redirect('/static/pacman/index.html');
+  },
+};
 
 export const gameController = {
   getGame(req: express.Request, res: express.Response) {
-    console.log('Controller was called');
-
     const internalGameName: string = req.params['game_name'];
-    pacman(req, res);
+    const game = gameServes[internalGameName];
+    game(req, res);
   },
 };
