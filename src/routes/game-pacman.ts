70db9ccbe65a4ad0bb47767178a0246b5e7d3d42
Carolyn Mei <mcarolyn@google.com>
2019-08-15 15:42:38 -0700

    Delete old route file

--- src/routes/game-pacman.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router: express.Router = express.Router();
-import { gameController } from '../controllers/gameController';
-
-router.get('/game/pacman', gameController.pacman);
-
-export const pacman = router;
