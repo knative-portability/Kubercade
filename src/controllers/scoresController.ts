593822fe494d1b2a0412da4ab5056f69f0a88540
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-16 15:29:45 -0700

    sort scores by datetime also

--- src/controllers/scoresController.ts
+++ src/controllers/scoresController.ts
@@ -25,7 +25,7 @@ async function getScoresFromDB(gameIndex: number) {
       `SELECT * 
       FROM kubercade.high_score_table
       WHERE game_index=$1
-      ORDER BY score DESC`,
+      ORDER BY datetime DESC, score DESC;`,
       [gameIndex]
     );
     return res.rows;
