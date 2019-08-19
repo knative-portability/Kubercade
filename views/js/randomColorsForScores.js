3fd29be8747d2ab7232197a3afe942dc4bbff8d5
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-19 16:14:00 -0700

    Random colors with min-bounded random rgb components

--- views/js/randomColorsForScores.js
+++ views/js/randomColorsForScores.js
@@ -1,15 +1,18 @@
 window.onload = function () {
   document.querySelectorAll("#high_scores_container .individual_score").forEach(function (element) {
-    var minLightnessRatio = 0.6;
-    var maxColorValue = 16777215; // 0xFFFFFFF
-    var color = getRandomIntInclusive(
-      maxColorValue * minLightnessRatio, maxColorValue);
-    element.style.color = Math.floor(color).toString(16);
+    var minLightnessRatio = 0.25;
+    var red = getRandomArbitrary(minLightnessRatio, 1);
+    var green = getRandomArbitrary(minLightnessRatio, 1);
+    var blue = getRandomArbitrary(minLightnessRatio, 1);
+    // scale colors to max of 255
+    var maxColor = Math.max(red, green, blue);
+    red = Math.floor(red / maxColor * 255);
+    green = Math.floor(green / maxColor * 255);
+    blue = Math.floor(blue / maxColor * 255);
+    element.style.color = `rgb(${red}, ${green}, ${blue})`;
   });
 }
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
