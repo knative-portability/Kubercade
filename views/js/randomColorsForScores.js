c74b0b10e985cb63751942b36b23a145d63e56ff
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-19 16:34:14 -0700

    Random color with hsl

--- views/js/randomColorsForScores.js
+++ views/js/randomColorsForScores.js
@@ -1,18 +1,6 @@
 window.onload = function () {
   document.querySelectorAll("#high_scores_container .individual_score").forEach(function (element) {
-    var minLightnessRatio = 0.25;
-    var red = getRandomArbitrary(minLightnessRatio, 1);
-    var green = getRandomArbitrary(minLightnessRatio, 1);
-    var blue = getRandomArbitrary(minLightnessRatio, 1);
-    // scale colors to max of 255
-    var maxColor = Math.max(red, green, blue);
-    red = Math.floor(red / maxColor * 255);
-    green = Math.floor(green / maxColor * 255);
-    blue = Math.floor(blue / maxColor * 255);
-    element.style.color = `rgb(${red}, ${green}, ${blue})`;
+    var hue = Math.floor(Math.random() * 360);
+    element.style.color = `hsl(${hue}, 100%, 65%)`;
   });
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
 }
