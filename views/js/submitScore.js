fb33e27dc06760ec7d31f7bc8723232a5ffa6279
Carolyn Mei <mcarolyn@google.com>
2019-08-19 14:07:26 -0700

    Get score from pacman game

--- views/js/submitScore.js
+++ views/js/submitScore.js
@@ -1,8 +1,14 @@
 var scorePopUp = function () {
+  var iframeLoc = document.getElementById(
+    "kubercade_iframe").contentWindow.location.href;
+  if (! iframeLoc.endsWith("games/pacman/")) {
+    alert("You can't submit a score if you aren't playing a game!");
+    return;
+  }
   var name = prompt("Please enter your name:", "anonymous");
   // don't post if person cancels prompt or doesn't enter name value
   if (name == null || name == "") {
-    return None;
+    return;
   }
   var score = getScore();
   console.log(score);
@@ -14,9 +20,8 @@ var scorePopUp = function () {
 }
 
 var getScore = function () {
-  // TODO: retrieve score from pacman game instead of returning fake value
-  // var iframe = document.getElementById("kubercade_iframe");
-  // var iframeContent = iframe.contentWindow;
-  // return iframeContent.getElementById("score");
-  return 15;
+  var iframe = document.getElementById("kubercade_iframe");
+  var scoreDiv = iframe.contentDocument.getElementById("score");
+  var score = scoreDiv.getElementsByTagName("span")[0].innerText;
+  return parseInt(score);
 }
