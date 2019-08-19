894cf085814e8634cdc64de2f4ef21b37e24c535
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-19 15:52:56 -0700

    Implement random color with minimum brightness on rng

--- views/js/randomColorsForScores.js
+++ views/js/randomColorsForScores.js
@@ -1,9 +1,15 @@
 window.onload = function () {
   document.querySelectorAll("#high_scores_container .individual_score").forEach(function (element) {
+    var minLightnessRatio = 0.6;
     var maxColorValue = 16777215; // 0xFFFFFFF
-    var baseColor = Math.random() * 16777215;
-    var lightnessMultiplier = 4;
-    var lightenedColor = (1 / lightnessMultiplier) * (baseColor - maxColorValue) + maxColorValue;
-    element.style.color = Math.floor(lightenedColor).toString(16);
+    var color = getRandomIntInclusive(
+      maxColorValue * minLightnessRatio, maxColorValue);
+    element.style.color = Math.floor(color).toString(16);
   });
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
