56fee0f63c13049a243eaaafdde8d2fd34ddfa47
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-19 15:48:15 -0700

    Implement lightness using brightness transform after generating color

--- views/js/randomColorsForScores.js
+++ views/js/randomColorsForScores.js
@@ -1,5 +1,9 @@
 window.onload = function () {
   document.querySelectorAll("#high_scores_container .individual_score").forEach(function (element) {
-    element.style.color = Math.floor(Math.random() * 16777215).toString(16);
+    var maxColorValue = 16777215; // 0xFFFFFFF
+    var baseColor = Math.random() * 16777215;
+    var lightnessMultiplier = 4;
+    var lightenedColor = (1 / lightnessMultiplier) * (baseColor - maxColorValue) + maxColorValue;
+    element.style.color = Math.floor(lightenedColor).toString(16);
   });
 }
