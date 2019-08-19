61616b5f45b9ce3a95a8091dba0aba7476005a0d
Carolyn Mei <mcarolyn@google.com>
2019-08-19 10:25:12 -0700

    Create score popup with name submit

---
+++ views/js/submitScore.js
@@ -0,0 +1,18 @@
+var scorePopUp = function () {
+  var name = prompt("Please enter your name:", "anonymous");
+  var score = getScore();   //TODO: get actual score
+  console.log(score);
+  const xhr = new XMLHttpRequest();
+  const data = `name=${name}&score=${score}`
+  xhr.open("POST", "/scores/pacman/")
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(data);
+}
+
+var getScore = function () {
+  // TODO: retrieve score from pacman game instead of returning fake value
+  // var iframe = document.getElementById("kubercade_iframe");
+  // var iframeContent = iframe.contentWindow;
+  // return iframeContent.getElementById("score");
+  return 15;
+}
