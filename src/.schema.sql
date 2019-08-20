ed268d4eeabc2c6146626c9dfa52cbe57a6bc533
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-20 14:58:26 -0700

    Add primary keys to schecma

--- src/.schema.sql
+++ src/.schema.sql
@@ -4,12 +4,14 @@ CREATE TABLE IF NOT EXISTS kubercade.high_score_table (
   game_index  integer,
   name        varchar(80) NOT NULL,
   score       integer NOT NULL,
-  datetime    timestamptz NOT NULL
+  datetime    timestamptz NOT NULL,
+  PRIMARY KEY (game_index, datetime)
 );
 
 CREATE TABLE IF NOT EXISTS kubercade.chat_table (
   game_index  integer,
   name        varchar(80) NOT NULL,
   message     varchar(400) NOT NULL,
-  datetime    timestamptz NOT NULL
+  datetime    timestamptz NOT NULL,
+  PRIMARY KEY (game_index, datetime)
 );
