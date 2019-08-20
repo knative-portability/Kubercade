c0a4a6ca49ae7487311e3401a46d2749b20c1f40
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-20 10:31:39 -0700

    Fix schema (?)

--- src/.schema.sql
+++ src/.schema.sql
@@ -1,15 +1,15 @@
-CREATE SCHEMA IF NOT EXIST kubercade AUTHORIZATION jxzxrgmv
+CREATE SCHEMA IF NOT EXISTS kubercade AUTHORIZATION cbkpwcnlqdxpfd;
 
   CREATE TABLE IF NOT EXISTS kubercade.high_score_table (
     game_index  integer,
-    name        varchar(80),
+    name        varchar(80) NOT NULL,
     score       integer NOT NULL,
     datetime    varchar(40) NOT NULL
   );
 
   CREATE TABLE IF NOT EXISTS kubercade.chat_table (
     game_index  integer,
-    name        varchar(80),
+    name        varchar(80) NOT NULL,
     message     varchar(400) NOT NULL,
     datetime    varchar(40) NOT NULL
   );
