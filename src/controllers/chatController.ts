173c25494036e1c5d96718aca4fec7caa2a4a4f7
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-21 13:56:51 -0700

    Unspecify 'internal' for chat gameName var (unmerged from #20)

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -18,8 +18,8 @@ export const chatController = {
    * @param res Response object
    */
   async getChat(req: express.Request, res: express.Response) {
-    const internalGameName: string = req.params['game_name'];
-    const gameIndex: number = gameInfoUtil.gameToIndex(internalGameName);
+    const gameName: string = req.params['game_name'];
+    const gameIndex: number = gameInfoUtil.gameToIndex(gameName);
     res.send(await getChatFromDB(gameIndex));
   },
   /**
@@ -29,8 +29,8 @@ export const chatController = {
    * @param res Response object
    */
   async postToChat(req: express.Request, res: express.Response) {
-    const internalGameName: string = req.params['game_name'];
-    const gameIndex: number = gameInfoUtil.gameToIndex(internalGameName);
+    const gameName: string = req.params['game_name'];
+    const gameIndex: number = gameInfoUtil.gameToIndex(gameName);
     const name: string = req.body.name || 'anonymous';
     const message: string = req.body.message;
     postChatMessageToDB(gameIndex, name, message);
