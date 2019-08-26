7b2e8a175899543be2ecee0b56df4ffbbc44988f
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-26 15:58:42 -0700

    store 2048 gamemanager in window

--- vendor/2048/js/application.js
+++ vendor/2048/js/application.js
@@ -1,4 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  window.gameManagerInstance =
+    new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
