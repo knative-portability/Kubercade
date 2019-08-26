91d8071fe9e075158ae6edc8f6a3758682293615
Gabe Mukobi <gabrielmukobi@google.com>
2019-08-26 14:27:43 -0700

    Improve game over message

--- views/js/submitScore.js
+++ views/js/submitScore.js
@@ -19,11 +19,9 @@ const getPacmanScore = (iframe) => {
 }
 
 const scorePopUp = (score, scoreUrl) => {
-  const name = prompt("Please enter your name:", "anonymous");
+  const name = prompt(`Congratulations, you scored ${score}! Please enter your name:`, 'anonymous');
   // don't post if person cancels prompt or doesn't enter name value
-  if (!name) {
-    return;
-  }
+  if (!name) return;
   sendScore(scoreUrl, {
     name,
     score
